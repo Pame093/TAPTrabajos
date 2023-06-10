e880c7ad559e9f960ed1503dbb86dbadaa4db351
pame <pamefarfan03@gmail.com>
2023-06-09 19:57:04 -0500

    Microservicios

---
+++ Microservicios/departament-service/src/main/java/com/loxasoft/departamentservice/entity/Departament.java
@@ -0,0 +1,19 @@
+package com.loxasoft.departamentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "departaments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Departament {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String code;
+}
